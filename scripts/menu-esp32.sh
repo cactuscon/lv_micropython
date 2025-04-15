9732888cf9c5de3ae081c8591667a7897f595c5f
rx13 <rx13@users.noreply.github.com>
2025-04-15 14:19:39 +0000

    CI Build Test Bump

--- scripts/menu-esp32.sh
+++ scripts/menu-esp32.sh
@@ -4,15 +4,7 @@
 
 # Array of configurations
 configs=(
-    "ESP32_GENERIC"
-    "ESP32_GENERIC SPIRAM"
-    "ESP32_GENERIC_S3"
-    "ESP32_GENERIC_S3 SPIRAM_OCT"
-    "M5STACK_CORE2"
-    "LILYGO_TDISPLAY"
-    "LILYGO_TWATCH_2020 V1"
-    "LILYGO_TWATCH_2020 V2"
-    "LILYGO_TWATCH_2020 V3"
+    "ESP32_GENERIC_S3_LVGL"
 )
 
 # Function to display menu and get user choice
